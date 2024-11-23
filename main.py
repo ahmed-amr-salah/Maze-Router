dccb806d15d7d7b5b4cfad9b9e0952bc1aa4d38b
Ahmed Amr <aamr@getocto.io>
2024-11-23 02:24:51 +0200

    Changing main to take arguments dynamically

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import sys
 from parser import parse_maze_router_input
 from output_display import print_input_data
 from algorithm import route_all_nets
 from output_display import print_routing_result, write_routing_results_to_file
 
-
 if __name__ == "__main__":
     try:
-        input_path = "input.txt"
-        output_path = "output.txt"
+        if len(sys.argv) != 3:
+            print("Usage: python main.py <input_path> <output_path>")
+            sys.exit(1)
+        
+        input_path = sys.argv[1]
+        output_path = sys.argv[2]
+
         router_input = parse_maze_router_input(input_path)
         print_input_data(router_input)
         routing_results = route_all_nets(router_input)
@@ -19,3 +24,4 @@ if __name__ == "__main__":
 
     except Exception as e:
         print(f"Error: {e}")
+        sys.exit(1)
